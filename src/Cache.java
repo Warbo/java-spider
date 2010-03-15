39c549be56b8214be217fe5ad071c1bb0961191e
unknown <Pha06cw@.windows.dcs.shef.ac.uk>
2010-03-15 14:19:54 +0000

    Restored Cache contents

--- src/Cache.java
+++ src/Cache.java
@@ -10,32 +10,61 @@
  */
 
 import java.util.*;
+import java.io.*;
+import java.net.*;
 
 public class Cache {
 
 	// Use HashTables for our storage, since they're O(1)
-	private HashTable robots;
-	private HashTable links_to;
-	private HashTable links_from;
-	private HashTable data;
-	private HashTable last_checked;
+	private Map<URL, Boolean> in_robots;
+	private Map<URL, List<URL>> links_to;
+	private Map<URL, List<URL>> links_from;
+	private Map<URL, String> data;
+	private Map<URL, Integer> last_checked;
+	private List<URL> unprocessed;
 
 	/*
 	 * Make a new, empty cache.
 	 */
 	public Cache() {
 		//TODO: Put templates here!
-		robots = new HashTable();
-		links_to = new HashTable();
-		links_from = new HashTable();
-		data = new HashTable();
-		last_checked = new HashTable();
+		in_robots = new Hashtable<URL, Boolean>();
+		links_to = new Hashtable<URL, List<URL>>();
+		links_from = new Hashtable<URL, List<URL>>();
+		data = new Hashtable<URL, String>();
+		last_checked = new Hashtable<URL, Integer>();
+		unprocessed = new LinkedList<URL>();
 	}
 
 	/*
 	 * Returns a URL from the cache which has not yet been processed.
 	 */
-	public get_unprocessed() {
+	public URL get_unprocessed() {
+		return null;
 	}
 
+	public static URL get_domain(URL currentURL) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean checked_robots(URL currentURL) {
+		return in_robots.containsKey(currentURL);
+	}
+
+	public boolean in_robots(URL currentURL) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List<URL> get_local_urls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<URL> get_external_urls() {
+		return null;
+	}
+
+}
 	
