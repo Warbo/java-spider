9b284da51bbabaf17103d40b71caae22dbac70bd
unknown <Pha06cw@.windows.dcs.shef.ac.uk>
2010-03-15 19:24:34 +0000

    Tracking FinishedException.java

---
+++ src/FinishedException.java
@@ -0,0 +1,10 @@
+/**
+ * Thrown by a Spider when it has exhausted its URLs
+ */
+
+/**
+ * If thrown, indicates that a Spider has run out of Web
+ */
+public class FinishedException extends Exception {
+
+}
